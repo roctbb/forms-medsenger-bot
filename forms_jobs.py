f171101e9a5b5a800328cabd69f1260b1558f9ca
roctbb <roctbb@gmail.com>
2021-03-10 16:39:46 +0300

    add wsgi support

--- forms_jobs.py
+++ forms_jobs.py
@@ -2,5 +2,5 @@ from forms_bot import *
 from apscheduler.schedulers.background import BlockingScheduler
 
 scheduler = BlockingScheduler()
-scheduler.add_job(timetable_manager.iterate(app), 'interval', minutes=5)
+scheduler.add_job(timetable_manager.iterate, 'interval', minutes=5, args=(app, ))
 scheduler.start()
