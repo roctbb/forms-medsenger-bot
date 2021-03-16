087a591a742fbbcfcae388b068c592e1bbdf2ca8
roctbb <roctbb@gmail.com>
2021-03-16 16:55:12 +0300

    add simple graphs

--- helpers.py
+++ helpers.py
@@ -72,8 +72,13 @@ def verify_json(func):
     return wrapper
 
 def get_ui(page, contract, categories='[]', object_id = None):
-    return render_template('index.html', page=page, object_id=object_id, contract_id=contract.id, api_host=MAIN_HOST.replace('8001', '8000'), local_host=LOCALHOST, agent_token=contract.agent_token, agent_id=AGENT_ID, categories=json.dumps(categories), is_admin=str(bool(contract.is_admin)).lower())
+    return render_template('index.html', page=page, object_id=object_id, contract_id=contract.id, api_host=MAIN_HOST.replace('8001', '8000'), local_host=LOCALHOST, agent_token=contract.agent_token, agent_id=AGENT_ID, categories=json.dumps(categories), is_admin=str(bool(contract.is_admin)).lower(), lc=dir_last_updated('static'))
 
 def delayed(delay, f, args):
     timer = threading.Timer(delay, f, args=args)
     timer.start()
+
+def dir_last_updated(folder):
+    return str(max(os.path.getmtime(os.path.join(root_path, f))
+                   for root_path, dirs, files in os.walk(folder)
+                   for f in files))
