b069a45674ede239b2432dd4b1d46ef3d400091d
roctbb <roctbb@gmail.com>
2021-04-20 18:17:00 +0300

    add sentry integration

--- manage.py
+++ manage.py
@@ -7,15 +7,15 @@ import sentry_sdk
 
 from config import *
 
-app = Flask(__name__)
-db_string = "postgresql://{}:{}@{}:{}/{}".format(DB_LOGIN, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE)
-app.config['SQLALCHEMY_DATABASE_URI'] = db_string
-
 sentry_sdk.init(
     dsn=SENTRY,
     integrations=[FlaskIntegration()],
     traces_sample_rate=0.0,
 )
+
+app = Flask(__name__)
+db_string = "postgresql://{}:{}@{}:{}/{}".format(DB_LOGIN, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_string
 
 db.init_app(app)
 
