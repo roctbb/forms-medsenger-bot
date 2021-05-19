7d1909388e51391a3285d4918b4f51e8d98bd62c
roctbb <roctbb@gmail.com>
2021-05-19 16:09:03 +0300

    add hidden descriptions

--- src/components/editors/parts/Criteria.vue
+++ src/components/editors/parts/Criteria.vue
@@ -54,26 +54,26 @@
                     </select>
                 </div>
 
-                    <div class="col-md-2" v-if="!['time', 'init'].includes(criteria.left_mode)">
-                        <select class="form-control form-control-sm" v-model="criteria.right_mode">
-                            <option value="value">фиксированное значение</option>
-                            <option value="category_value">значение</option>
-                            <option value="average" v-if="is_int()">среднее за</option>
-                            <option value="sum" v-if="is_int()">сумма за</option>
-                            <option value="difference" v-if="is_int()">разброс за</option>
-                        </select>
-                    </div>
+                <div class="col-md-2" v-if="!['time', 'init'].includes(criteria.left_mode)">
+                    <select class="form-control form-control-sm" v-model="criteria.right_mode">
+                        <option value="value">фиксированное значение</option>
+                        <option value="category_value">значение</option>
+                        <option value="average" v-if="is_int()">среднее за</option>
+                        <option value="sum" v-if="is_int()">сумма за</option>
+                        <option value="difference" v-if="is_int()">разброс за</option>
+                    </select>
+                </div>
 
-                    <div class="col-md-2" v-if="criteria.right_mode != 'value' && !['time', 'init'].includes(criteria.left_mode)">
-                        <select class="form-control form-control-sm"
-                                v-model="criteria.right_category">
-                            <optgroup
-                                v-for="(group, name) in group_by(category_list, 'subcategory')"
-                                v-bind:label="name">
-                                <option v-for="category in group" :value="category.name">{{ category.description }}
-                                </option>
-                            </optgroup>
-                        </select>
+                <div class="col-md-2" v-if="criteria.right_mode != 'value' && !['time', 'init'].includes(criteria.left_mode)">
+                    <select class="form-control form-control-sm"
+                            v-model="criteria.right_category">
+                        <optgroup
+                            v-for="(group, name) in group_by(category_list, 'subcategory')"
+                            v-bind:label="name">
+                            <option v-for="category in group" :value="category.name">{{ category.description }}
+                            </option>
+                        </optgroup>
+                    </select>
 
                     <small class="text-muted">Код категории для сравнения</small>
                 </div>
@@ -129,6 +129,13 @@
                 </div>
             </div>
 
+            <div v-if="is_admin" class="row">
+                <div class="col-md-12">
+                    <input type="checkbox" v-model="criteria.hide_in_description">
+                    <small class="text-muted">Не выводить в показателях?</small>
+                </div>
+            </div>
+
             <div v-if="is_admin && criteria.left_mode !='init' && ['value', 'category_value'].includes(criteria.right_mode)" class="row">
                 <div class="col-md-4">
                     <input type="checkbox" v-model="criteria.ask_value">
@@ -145,13 +152,6 @@
                            :class="this.save_clicked && empty(criteria.value_code) ? 'is-invalid' : ''"
                            v-model="criteria.value_code">
                     <small class="text-muted">Код (для сценариев)</small>
-                </div>
-            </div>
-
-            <div v-if="is_admin">
-                <div class="col-md-12">
-                    <input type="checkbox" v-model="criteria.hide_in_description">
-                    <small class="text-muted">Не выводить в показателях?</small>
                 </div>
             </div>
         </div>
