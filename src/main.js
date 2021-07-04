634dc504a6838fde4f81d50bcc6fe077c2eb1475
Nadia Laban <naabulaban@gmail.com>
2021-07-04 23:03:02 +0300

    change icon size

--- src/main.js
+++ src/main.js
@@ -223,7 +223,7 @@ Vue.mixin({
             },
             images: {
                 form: window.LOCAL_HOST + '/static/images/icons8-fill-in-form-48.png',
-                warning: window.LOCAL_HOST + '/static/images/icons8-error-24.png',
+                warning: window.LOCAL_HOST + '/static/images/icons8-error-18.png',
                 medicine: window.LOCAL_HOST + '/static/images/icons8-pill-96.png',
                 algorithm: window.LOCAL_HOST + '/static/images/icons8-artificial-intelligence-96.png',
                 ok: window.LOCAL_HOST + '/static/images/icons8-ok-128.png',
