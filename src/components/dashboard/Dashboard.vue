032f77a296b925db77d96ed39aaffd1072917dd3
roctbb <roctbb@gmail.com>
2021-08-31 14:25:37 +0300

    preview_form

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -445,9 +445,6 @@ export default {
                 }
             )
         },
-        preview_form_url: function (form) {
-            return this.url('/preview_form/' + form.id)
-        },
         process_delete_form_answer: function (response) {
             if (response.data.deleted_id) {
                 this.patient.forms = this.patient.forms.filter(f => f.id != response.data.deleted_id)
@@ -1008,7 +1005,7 @@ export default {
             )
         },
         preview_form_url: function (form) {
-            return this.url('/form/' + form.id)
+            return this.url('/preview_form/' + form.id)
         },
         process_delete_form_answer: function (response) {
             if (response.data.deleted_id) {
