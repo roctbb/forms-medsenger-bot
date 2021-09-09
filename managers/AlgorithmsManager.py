0458298dff97e610b19298c2e10a59c856a98f5c
roctbb <roctbb@gmail.com>
2021-09-09 11:42:19 +0300

    fix init_step actions

--- managers/AlgorithmsManager.py
+++ managers/AlgorithmsManager.py
@@ -722,11 +722,12 @@ class AlgorithmsManager(Manager):
         return True
 
     def check_inits(self, algorithm, contract):
-        for condition in algorithm.common_conditions:
-            if any(
-                any(criteria['category'] == 'init' for criteria in block) for block in condition['criteria']):
-                for action in condition['positive_actions']:
-                    self.run_action(action, contract.id, [], algorithm)
+        if algorithm.common_conditions:
+            for condition in algorithm.common_conditions:
+                if any(
+                    any(criteria['category'] == 'init' for criteria in block) for block in condition['criteria']):
+                    for action in condition['positive_actions']:
+                        self.run_action(action, contract.id, [], algorithm)
 
         for step in algorithm.steps:
             for condition in step['conditions']:
