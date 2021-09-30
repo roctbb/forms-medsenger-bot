6623067076040d672405ec1b958670ae498d8524
roctbb <roctbb@gmail.com>
2021-09-30 14:10:05 +0300

    add print exception

--- forms_bot.py
+++ forms_bot.py
@@ -123,7 +123,6 @@ def init(data):
                 print(e)
 
         custom_algorithms = filter(lambda x: "algorithm_" in x and params.get(x), params.keys())
-        print(list(custom_algorithms))
         for custom_algorithm in custom_algorithms:
             try:
                 algorithm_id = int(custom_algorithm.split('_')[1])
