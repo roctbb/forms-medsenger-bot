94964810d5e37700d2efda2defe6cc2e09989863
Nadia Laban <naabulaban@gmail.com>
2021-10-20 14:22:26 +0300

    change medicine confirmation page

--- src/components/presenters/ConfirmMedicinePresenter.vue
+++ src/components/presenters/ConfirmMedicinePresenter.vue
@@ -1,33 +1,57 @@
 <template>
     <div>
-        <div v-if="data.length" style="margin-bottom: 15px;">
-            <h5>Назначенные лекарства</h5>
-            <p>Выберите лекарство, прием которого Вы хотите записать.</p>
-            <div class="row">
-                <div class="col-4" v-for="(medicine, i) in data" style="padding-bottom: 10px;">
-                    <button class="btn btn-primary btn-block" @click="save(medicine)">{{ medicine.title }}</button>
+        <div v-if="medicines.length" style="margin-bottom: 15px;">
+            <div v-if="!custom">
+                <h5>Записать прием лекарства</h5>
+                <div style="margin-top: 15px;" class="alert alert-info" role="alert">
+                    <p>Напоминания о плановом приеме лекарств приходят согласно расписанию.
+                        Если Вы уже отметили прием препарата с помощью кнопки в чате, поваторно записвать прием не нужно.</p>
                 </div>
+
+                <p>Выберите лекарство, прием которого Вы хотите записать.</p>
+                <div class="row" v-if="!mobile">
+                    <card v-for="(medicine, i) in medicines" :key="medicine.id" :image="images.medicine" class="col-lg-6 col-md-7">
+                        <h6>{{ medicine.title }}</h6>
+                        <small><strong>Назначенная дозировка: </strong> {{ medicine.dose ? medicine.dose : '-' }}</small><br>
+                        <small><strong>Правила приема: </strong> {{ medicine.rules ? medicine.rules : '-' }}</small><br>
+                        <small><i>{{ tt_description(medicine.timetable) }}</i></small><br>
+                        <button class="btn btn-success btn-sm" @click="save(medicine)">Записать прием</button>
+                    </card>
+                </div>
+                <div style="padding-bottom: 10px;" v-else>
+                    <card v-for="(medicine, i) in medicines" :key="medicine.id" :image="images.medicine">
+                        <h6>{{ medicine.title }}</h6>
+                        <small><strong>Назначенная дозировка: </strong> {{ medicine.dose ? medicine.dose : '-' }}</small><br>
+                        <small><strong>Правила приема: </strong> {{ medicine.rules ? medicine.rules : '-' }}</small><br>
+                        <small><i>{{ tt_description(medicine.timetable) }}</i></small><br>
+                        <button class="btn btn-success btn-sm" @click="save(medicine)">Записать прием</button>
+                    </card>
+                </div>
+                <button class="btn btn-primary" @click="custom = true">Другое лекарство</button>
+            </div>
+            <div v-else>
+                <h5>Другое лекарство</h5>
+                <div>
+                    <form-group48 title="Название лекарства">
+                        <input class="form-control form-control-sm"
+                               :class="save_clicked && empty(custom_medicine.title) ? 'is-invalid' : ''"
+                               v-model="custom_medicine.title"/>
+                    </form-group48>
+
+                    <form-group48 title="Принятая доза">
+                        <input class="form-control form-control-sm"
+                               :class="save_clicked && empty(custom_medicine.dose) ? 'is-invalid' : ''"
+                               v-model="custom_medicine.dose"/>
+                    </form-group48>
+
+                    <form-group48 title="Комментарий">
+                        <textarea class="form-control monitoring-input" v-model="custom_medicine.comment"/>
+                    </form-group48>
+                </div>
+                <button class="btn btn-danger btn-sm" @click="custom = false">Назад</button>
+                <button class="btn btn-success btn-sm" @click="custom_save()">Записать прием</button>
             </div>
         </div>
-        <h5>Другое лекарство</h5>
-        <div>
-            <form-group48 title="Название лекарства">
-                <input class="form-control form-control-sm"
-                       :class="save_clicked && empty(custom_medicine.title) ? 'is-invalid' : ''"
-                       v-model="custom_medicine.title"/>
-            </form-group48>
-
-            <form-group48 title="Принятая доза">
-                <input class="form-control form-control-sm"
-                       :class="save_clicked && empty(custom_medicine.dose) ? 'is-invalid' : ''"
-                       v-model="custom_medicine.dose"/>
-            </form-group48>
-
-            <form-group48 title="Комментарий">
-                <input class="form-control form-control-sm" v-model="custom_medicine.comment"/>
-            </form-group48>
-        </div>
-        <button class="btn btn-success" @click="custom_save()">Записать прием</button>
         <error-block :errors="errors"/>
 
     </div>
@@ -36,11 +60,12 @@
 <script>
 import ActionDone from "./ActionDone";
 import FormGroup48 from "../common/FormGroup-4-8";
+import Card from "../common/Card";
 import ErrorBlock from "../common/ErrorBlock";
 
 export default {
     name: "ConfirmMedicinePresenter",
-    components: {ActionDone, ErrorBlock, FormGroup48},
+    components: {ActionDone, ErrorBlock, FormGroup48, Card},
     props: {
         data: {
             required: false
@@ -51,6 +76,7 @@ export default {
             medicines: {},
             custom_medicine: {},
             save_clicked: false,
+            custom: false,
             errors: []
         }
     },
@@ -86,14 +112,17 @@ export default {
                 }
                 this.axios.post(this.url('/api/confirm-medicine'), data).then(r => Event.fire('confirm-medicine-done')).catch(r => this.errors.push('Ошибка сохранения'));
             }
-        },
-        created() {
-            this.medicines = this.data
-            this.custom_medicine = {
-                title: '',
-                dose: '',
-                comment: ''
-            }
+        }
+    },
+    mounted() {
+        this.medicines = this.data
+        this.medicines.sort((a, b) => {
+            return a.title < b.title ? -1 : a.title > b.title ? 1 : 0
+        })
+        this.custom_medicine = {
+            title: '',
+            dose: '',
+            comment: ''
         }
     }
 }
