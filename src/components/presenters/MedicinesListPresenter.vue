74704386101e28793731c3f74e026f99c425f396
roctbb <roctbb@gmail.com>
2021-12-08 11:46:17 +0300

    add medicine notifications disable

--- src/components/presenters/MedicinesListPresenter.vue
+++ src/components/presenters/MedicinesListPresenter.vue
@@ -2,14 +2,19 @@
     <div v-if="medicines">
         <h3>Назначенные лекарства</h3>
         <div class="row">
-            <card v-for="(medicine, i) in medicines" :key="medicine.id" :image="images.medicine" class="col-lg-6 col-md-7">
+            <card v-for="(medicine, i) in medicines" :key="medicine.id" :image="images.medicine"
+                  class="col-lg-6 col-md-7">
                 <h6>{{ medicine.title }}</h6>
                 <small><strong>Дозировка: </strong> {{ medicine.dose ? medicine.dose : '-' }}</small><br>
                 <small><strong>Правила приема: </strong> {{ medicine.rules ? medicine.rules : '-' }}</small><br>
-                <small><i>{{ tt_description(medicine.timetable) }}</i></small><br>
-                <small v-if="medicine.notifications_disabled"><strong>Уведомления выключены</strong><br></small>
-                <a href="#" v-if="!medicine.notifications_disabled" @click="disable_notifications(medicine)">Отключить уведомления</a>
-                <a href="#" v-if="medicine.notifications_disabled" @click="enable_notifications(medicine)">Включить уведомления</a>
+                <small><i>{{ tt_description(medicine.timetable) }}</i></small>
+                <div v-if="medicine.timetable.mode !='manual'">
+                    <small v-if="medicine.notifications_disabled"><strong>Уведомления выключены</strong><br></small>
+                    <a href="#" v-if="!medicine.notifications_disabled" @click="disable_notifications(medicine)">Отключить
+                        уведомления</a>
+                    <a href="#" v-if="medicine.notifications_disabled" @click="enable_notifications(medicine)">Включить
+                        уведомления</a>
+                </div>
             </card>
         </div>
     </div>
