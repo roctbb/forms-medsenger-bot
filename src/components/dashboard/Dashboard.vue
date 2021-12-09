a8077042676a9d0a8099123f31ceefc1da11bb85
roctbb <roctbb@gmail.com>
2021-12-09 14:13:27 +0300

    add init reminder

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -385,7 +385,7 @@ export default {
     },
     methods: {
         reminder_duration: function (reminder) {
-            if (!reminder.attach_date || !reminder.detach_date) return '&#8734;'
+            if (!reminder.attach_date || !reminder.detach_date) return '∞'
             let attach = moment(reminder.attach_date, "YYYY-MM-DD")
             let detach = moment(reminder.detach_date, "YYYY-MM-DD")
             return moment.duration(detach.diff(attach)).asDays()
