84cec06c1231c190ebad0d4f0a18c405d6e98a07
roctbb <roctbb@gmail.com>
2023-02-03 01:28:08 +0300

    update conf

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ sudo cp agents_forms.conf /etc/supervisor/conf.d/
 sudo cp agents_forms_nginx.conf /etc/nginx/sites-enabled/
 sudo supervisorctl update
 sudo systemctl restart nginx
-sudo certbot --nginx -d forms.medsenger.ru
+sudo certbot --nginx -d forms.ai.medsenger.ru
 touch config.py
