d8b60c9e25428175dcc1cb8ddd2bee15a3ee8835
roctbb <roctbb@gmail.com>
2023-11-17 12:22:34 +0300

    check time for form and medicine warnings

--- forms_jobs.py
+++ forms_jobs.py
@@ -4,7 +4,7 @@ from apscheduler.schedulers.background import BlockingScheduler
 scheduler = BlockingScheduler()
 scheduler.add_job(timetable_manager.iterate, 'interval', minutes=1, args=(app, ))
 scheduler.add_job(algorithm_manager.check_timeouts, 'interval', minutes=1, args=(app, ))
-scheduler.add_job(timetable_manager.check_forgotten, 'interval', days=1, args=(app, ))
+scheduler.add_job(timetable_manager.check_forgotten, 'cron', hour=7, minute=1, second=0, args=(app, ))
 scheduler.add_job(timetable_manager.check_hours, 'interval', hours=1, args=(app, ))
 scheduler.add_job(timetable_manager.check_days, 'cron', hour=7, minute=1, second=0, args=(app, ))
 scheduler.add_job(timetable_manager.update_daily_tasks, 'cron', hour=3, minute=1, second=0, args=(app, ))
