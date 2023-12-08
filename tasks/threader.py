2e8ada49076ad5b0237b3b489c7907ceb91ea7d2
roctbb <roctbb@gmail.com>
2023-12-08 21:01:38 +0300

    fix wrong time for records

--- tasks/threader.py
+++ tasks/threader.py
@@ -3,11 +3,11 @@ from infrastructure import medsenger_api
 
 @celery.task
 def async_order(contract_id, order, agent_id=None, params=None):
-    medsenger_api.send_order(contract_id, order, agent_id, params)
+    medsenger_api.send_order(contract_id, order, agent_id, params=params)
 
 @celery.task
 def async_record(contract_id, category_name, value, params=None):
-    medsenger_api.add_record(contract_id, category_name, value, params)
+    medsenger_api.add_record(contract_id, category_name, value, params=params)
 
 @celery.task
 def async_add_hooks(contract_id, categories):
