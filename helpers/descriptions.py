cd575714485d81f63f82ee220c064182e559f510
roctbb <roctbb@gmail.com>
2023-12-11 14:05:44 +0300

    ai refactoring

--- helpers/descriptions.py
+++ helpers/descriptions.py
@@ -1,12 +1,5 @@
-def generate_event_description(criteria, l_value, r_value, category_names, current_answer):
-    if criteria.get('left_mode') == 'value' and criteria.get('right_mode') == 'value' and criteria.get('sign') in [
-        'equal', 'contains'] and current_answer:
-        if not current_answer.get('params', {}).get('type'):
-            return ""
-
-        return current_answer['value'] + "."
-
-    signs = {
+def get_signs():
+    return {
         "equal": "равно",
         "not_equal": "не равно",
         "greater": "больше",
@@ -22,7 +15,9 @@ def generate_event_description(criteria, l_value, r_value, category_names, curre
         "date_less_or_equal": "меньше или равно",
     }
 
-    left_modes = {
+
+def get_left_modes():
+    return {
         "value": "Значение ",
         "category_value": "Значение ",
         "sum": "Сумма ",
@@ -34,7 +29,9 @@ def generate_event_description(criteria, l_value, r_value, category_names, curre
         "min": "Минимальное значение "
     }
 
-    right_modes = {
+
+def get_right_modes():
+    return {
         "sum": "сумме",
         "difference": "разности крайних значений",
         "delta": "разбросу",
@@ -44,9 +41,17 @@ def generate_event_description(criteria, l_value, r_value, category_names, curre
         "min": "минимальному значению"
     }
 
-    LEFT_MODE = left_modes.get(criteria.get('left_mode'))
+
+def generate_event_description(criteria, l_value, r_value, category_names, current_answer):
+    if (criteria.get('left_mode') == 'value' and criteria.get('right_mode') == 'value'
+            and criteria.get('sign') in ['equal', 'contains'] and current_answer):
+        if not current_answer.get('params', {}).get('type'):
+            return ""
+        return current_answer['value'] + "."
+
+    LEFT_MODE = get_left_modes().get(criteria.get('left_mode'))
     LEFT_CATEGORY = category_names.get(criteria.get('category'))
-    SIGN = signs[criteria.get('sign')]
+    SIGN = get_signs()[criteria.get('sign')]
 
     if criteria.get('sign') not in ['equal', 'contains'] or criteria.get('left_mode') != 'value':
         comment = "{} '{}' (<strong>{}</strong>) {} ".format(LEFT_MODE, LEFT_CATEGORY, l_value, SIGN)
@@ -56,9 +61,8 @@ def generate_event_description(criteria, l_value, r_value, category_names, curre
     if criteria.get('right_mode') in ['value', 'category_value']:
         comment += "<strong>{}</strong>".format(criteria.get('value'))
     else:
-        comment += "{} за {} часа (ов) (<strong>{}</strong>)".format(right_modes[criteria.get('right_mode')],
-                                                                     criteria.get('right_hours'), r_value)
-
+        comment += "{} за {} часа (ов) (<strong>{}</strong>)".format(
+            get_right_modes()[criteria.get('right_mode')], criteria.get('right_hours'), r_value)
         if criteria.get('right_category'):
             comment += " '{}'".format(category_names.get(criteria.get('right_category')))
 
