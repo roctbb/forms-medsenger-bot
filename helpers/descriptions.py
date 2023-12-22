580be27f166699129a615bccd9d1be50f1b56e75
roctbb <roctbb@gmail.com>
2023-12-22 20:08:18 +0300

    Add exception handling and update event description generator
    In this commit, an exception handling has been introduced into `generate_event_description` function to ensure robustness. Also, the function has been updated to support new dimensions, and to deal with offsets in right-side criteria. A new helper function `dimensions` has been added to map keywords to localized strings.

--- helpers/descriptions.py
+++ helpers/descriptions.py
@@ -1,3 +1,6 @@
+from helpers import log
+
+
 def get_signs():
     return {
         "equal": "равно",
@@ -42,31 +45,50 @@ def get_right_modes():
     }
 
 
+def dimensions():
+    return {
+        "times": "раз",
+        "hours": "часов",
+        "days": "дней"
+    }
+
+
 def generate_event_description(criteria, l_value, r_value, category_names, current_answer):
-    if (criteria.get('left_mode') == 'value' and criteria.get('right_mode') == 'value'
-            and criteria.get('sign') in ['equal', 'contains'] and current_answer):
-        if not current_answer.get('params', {}).get('type'):
-            return ""
-        return current_answer['value'] + "."
+    try:
+        if (criteria.get('left_mode') == 'value' and criteria.get('right_mode') == 'value'
+                and criteria.get('sign') in ['equal', 'contains'] and current_answer):
+            if not current_answer.get('params', {}).get('type'):
+                return ""
+            return current_answer['value'] + "."
 
-    LEFT_MODE = get_left_modes().get(criteria.get('left_mode'))
-    LEFT_CATEGORY = category_names.get(criteria.get('category'))
-    SIGN = get_signs()[criteria.get('sign')]
+        LEFT_MODE = get_left_modes().get(criteria.get('left_mode'))
+        LEFT_CATEGORY = category_names.get(criteria.get('category'))
+        SIGN = get_signs()[criteria.get('sign')]
 
-    if criteria.get('sign') not in ['equal', 'contains'] or criteria.get('left_mode') != 'value':
-        comment = "{} '{}' (<strong>{}</strong>) {} ".format(LEFT_MODE, LEFT_CATEGORY, l_value, SIGN)
-    else:
-        comment = "{} '{}' {} ".format(LEFT_MODE, LEFT_CATEGORY, SIGN)
+        if criteria.get('sign') not in ['equal', 'contains'] or criteria.get('left_mode') != 'value':
+            comment = "{} '{}' (<strong>{}</strong>) {} ".format(LEFT_MODE, LEFT_CATEGORY, l_value, SIGN)
+        else:
+            comment = "{} '{}' {} ".format(LEFT_MODE, LEFT_CATEGORY, SIGN)
 
-    if criteria.get('right_mode') in ['value', 'category_value']:
-        comment += "<strong>{}</strong>".format(criteria.get('value'))
-    else:
-        comment += "{} за {} часа (ов) (<strong>{}</strong>)".format(
-            get_right_modes()[criteria.get('right_mode')], criteria.get('right_hours'), r_value)
-        if criteria.get('right_category'):
-            comment += " '{}'".format(category_names.get(criteria.get('right_category')))
+        if criteria.get('right_mode') in ['value', 'category_value']:
+            comment += "<strong>{}</strong>".format(criteria.get('value'))
+        else:
+            right_key = 'right_hours' if criteria.get('right_dimension') == "hours" else 'right_times'
 
-    return comment
+            comment += "{} за {} {} (<strong>{}</strong>)".format(
+                get_right_modes()[criteria.get('right_mode')], criteria.get(right_key),
+                dimensions()[criteria.get('right_dimension')], r_value)
+
+            if criteria.get('right_offset') and criteria.get('right_offset_dimension'):
+                comment += "с отступом в {} {}".format(criteria.get('right_offset'),
+                                                       dimensions()[criteria.get('right_offset_dimension')])
+
+            if criteria.get('right_category'):
+                comment += " '{}'".format(category_names.get(criteria.get('right_category')))
+
+        return comment
+    except Exception as e:
+        log(e, False)
 
 
 def generate_contract_description(contract):
