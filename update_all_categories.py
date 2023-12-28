ee18186ac53da5497d6e26688d3616e34eec7e68
roctbb <roctbb@gmail.com>
2023-12-28 12:37:26 +0300

    Fix bug in AlgorithmManager and improve null handling
    Revised code to correctly parameterize the run_algorithm function call in AlgorithmManager fixing a bug. Enhanced null checking in algorithm_runner to handle potential errors with 'category' and 'right_category' fields during categorization process.

--- update_all_categories.py
+++ update_all_categories.py
@@ -3,5 +3,8 @@ from models import *
 
 with app.app_context():
     for algorithm in Algorithm.query.all():
-        algorithm_manager.update_categories(algorithm)
+        try:
+            algorithm_manager.update_categories(algorithm)
+        except Exception as e:
+            print("Problem with algorithm {}: {}".format(algorithm.id, e))
     db.session.commit()
