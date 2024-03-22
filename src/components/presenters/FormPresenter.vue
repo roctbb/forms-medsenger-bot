b0c06dcd50c3a84163bb0719359ef47ed9474719
roctbb <roctbb@gmail.com>
2024-03-22 11:03:01 +0300

    Update range field validation in FormPresenter.vue
    Removed 'required' attribute from the 'range' input type to address incorrect form validation. Updated the inline value display to use bold instead of 'strong' for consistency within the application and to meet accessibility standards.

--- src/components/presenters/FormPresenter.vue
+++ src/components/presenters/FormPresenter.vue
@@ -94,7 +94,7 @@
                                          type="time"></date-picker>
                         </div>
 
-                        <input type="range" :min="field.params.min" :max="field.params.max" :step="field.params.step"
+                        <input type="range" :min="field.params.min" :max="field.params.max" :step="field.params.step" :value="field.params.default"
                                class="form-control monitoring-input"
                                :class="save_clicked && field.required && (!answers[field.uid] && answers[field.uid] !== 0) ? 'is-invalid' : ''"
                                v-if="field.type == 'range'" v-model="answers[field.uid]"/>
