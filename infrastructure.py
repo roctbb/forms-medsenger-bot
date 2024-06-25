41d11988e8e970026bc4a7a84f8cb92e30bfe3cb
roctbb <roctbb@gmail.com>
2024-06-25 17:50:27 +0300

    Update Celery worker command and set default queue
    The Celery worker command in agents_forms.conf has been updated to include a specified 'forms_queue'. A default task queue 'forms_queue' has also been set in the infrastructure.py file, thus ensuring all tasks will be routed to this queue.

--- infrastructure.py
+++ infrastructure.py
@@ -20,5 +20,7 @@ celery = Celery(
     backend="redis://127.0.0.1:6379/0"
 )
 
+celery.conf.task_default_queue = 'forms_queue'
+
 medsenger_api = AgentApiClient(API_KEY, MAIN_HOST, AGENT_ID, API_DEBUG, use_grpc=USE_GRPC, grpc_host=GRPC_HOST,
                                sentry_dsn=SENTRY)
